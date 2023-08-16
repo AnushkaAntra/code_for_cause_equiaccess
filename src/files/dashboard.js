f183d75c8349ed9e465694f18e7aa163fd653101
Anushka Antra <antraanushka31@gmail.com>
2023-08-17 02:59:41 +0530

    still working on dashboard

--- src/files/dashboard.js
+++ src/files/dashboard.js
@@ -33,15 +33,29 @@ const Dashboard = () => {
 						</span>
 						<span className='carousel-container d-flex ps-4'>
 						<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+							<div class="carousel-indicators">
+								<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true"></button>
+								<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></button>
+								<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></button>
+							</div>
 							<div class="carousel-inner">
 								<div class="carousel-item active">
 									<img src="https://www.handtalk.me/en/wp-content/uploads/sites/3/2023/05/191-capa-blog-post_accessibility_technology_deaf.png" class="d-block" width="550px" alt="learning" />
+									<div class="carousel-caption d-none d-md-block">
+										<h5>Learn with us</h5>
+									</div>
 								</div>
 								<div class="carousel-item">
 									<img src="https://www.handtalk.me/en/wp-content/uploads/sites/3/2023/07/capa-blogpost-especialistas_acessibilidade.webp" class="d-block" width="550px" alt="communicating" />
+									<div class="carousel-caption d-none d-md-block">
+										<h5>Communicate easily</h5>
+									</div>
 								</div>
 								<div class="carousel-item">
-									<img src="https://www.handtalk.me/en/wp-content/uploads/sites/3/2023/06/capa-blogpost-ai.png" class="d-block" width="550px" alt="practicing" />
+									<img src="https://www.handtalk.me/en/wp-content/uploads/sites/3/2023/06/capa-blogpost-ai.png" class="d-block" width="550px" alt="Assissting" />
+									<div class="carousel-caption d-none d-md-block">
+										<h5>Assisstive technology</h5>
+									</div>
 								</div>
 							</div>
 							<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
@@ -54,6 +68,25 @@ const Dashboard = () => {
 							</button>
 							</div>
 						</span>
+					</div>
+				</div>
+				<h2>How it works</h2>
+				<br />
+				<div className="work d-flex flex-row ps-4">
+					<div className="first ps-4">
+						<img className="i1" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT22WlWJbmrL9psK-dsblc2F5dCYhVgtXzmRw&usqp=CAU" alt="" />
+					</div>
+					<div className="arrow">
+						<img src="https://cdn-icons-png.flaticon.com/128/9692/9692032.png" width="40px" alt="arrow" />
+					</div>
+					<div className="second ps-4">
+						<img className="i1" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT22WlWJbmrL9psK-dsblc2F5dCYhVgtXzmRw&usqp=CAU" alt="" />  
+					</div>
+					<div className="arrow">
+						<img src="https://cdn-icons-png.flaticon.com/128/9692/9692032.png" width="40px" alt="arrow" />
+					</div>
+					<div className="third ps-4">
+						<img className="i1" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT22WlWJbmrL9psK-dsblc2F5dCYhVgtXzmRw&usqp=CAU" alt="" />
 					</div>
 				</div>
 			</Router>
